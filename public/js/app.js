6e8c167d31f98ecd33889bd67ea2bbc5798cf76b
Franciosi Francesco <francesco.franciosi@p-a.it>
2020-10-29 09:45:34 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,65 @@
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading......'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return messageTwo.textContent = data.error
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+
+        })
+    })
+
+})
+
+
+
+
+
+
+
+
+
+// fetch('http://localhost:3000/weather?address=boston').then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             return console.log(data.error)
+//         }
+//         console.log(data.location)
+//         console.log(data.forecast)
+//     })
+// })
+
+
+
+// fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,crypto-com-chain&vs_currencies=eur').then((response) => {
+//     if (!response) {
+//         return console.log('problem with response')
+//     }
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+// fetch('http://api.weatherstack.com/current?access_key=e57f40458cf33b647dfa6d6690478380&query=Bologna').then((response) => {
+//     if (!response) {
+//         return console.log('problem with response')
+//     }
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
---
+++ src/app.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlesbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Franz'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        name: 'Franz'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'Franz'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: ' you must provide a address '
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error: error
+            })
+        }
+
+        forecast(latitude, longitude, (error, forecastdata) => {
+            if (error) {
+                return res.send({
+                    error: error
+                })
+            }
+            res.send({
+                address: req.query.address,
+                forecast: forecastdata,
+                location: location
+            })
+        })
+    })
+
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Help article not found',
+        name: 'Franz'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Page not found',
+        name: 'Franz'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
